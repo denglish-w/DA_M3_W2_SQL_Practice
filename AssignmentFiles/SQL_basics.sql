20e29141c385d953e7bb66f6d741a8a7ac2ed9e9
David English <dwenglish97@outlook.com>
2025-10-07 12:48:54 -0400

    finished nine questions. passed over the tenth due to it needing a join and not entirely sure how to make a 'computed column' of total_items.

--- AssignmentFiles/SQL_basics.sql
+++ AssignmentFiles/SQL_basics.sql
@@ -29,17 +29,36 @@ WHERE name = 'Latte';
 
 -- Q5) Show distinct payment methods used in the dataset.
 
-
+SELECT DISTINCT payment_method
+FROM orders;
 
 -- Q6) For each store, list its name and city/state (one row per store).
+
+SELECT name, city, state
+FROM stores;
 
 -- Q7) From orders, show order_id, status, and a computed column total_items
 --     that counts how many items are in each order.
 
+
+
 -- Q8) Show orders placed on '2025-09-04' (any time that day).
+
+SELECT * 
+FROM orders
+WHERE order_datetime = 2025-09-04;
 
 -- Q9) Return the top 3 most expensive products (price, name).
 
+SELECT price, name
+FROM products 
+ORDER BY price DESC
+LIMIT 3;
+
 -- Q10) Show customer full names as a single column 'customer_name'
 --      in the format "Last, First".
+
+SELECT CONCAT(last_name, ', ', first_name) AS customer_name
+FROM customers;
+
 
